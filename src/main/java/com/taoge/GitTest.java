afdfdef68fcf029f38a79da6f57f7df2a910e106
cstaoge <117703626+cstaoge@users.noreply.github.com>
2022-11-08 17:28:52 +0800

    taoge commit

--- src/main/java/com/taoge/GitTest.java
+++ src/main/java/com/taoge/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4444!!");
         System.out.println("master test!!");
         System.out.println("push test!!");
+        System.out.println("taoge commit!!");
     }
 }
