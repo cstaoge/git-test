8c8af699eb97a8a35bb29288b879f59b3a98de33
root <root@toage.com>
2022-11-08 16:40:44 +0800

    master commit2

--- src/main/java/com/taoge/GitTest.java
+++ src/main/java/com/taoge/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git22!!");
         System.out.println("hello git333!!");
         System.out.println("hello git4444!!");
+        System.out.println("master test!!");
     }
 }
